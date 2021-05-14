8a97f2df4dcf9885c8b7a2e1d73f2bc0a47c3fc4
Johan Bové <johan@johanbove.info>
2021-05-15 00:26:24 +0200

    Update content

---
+++ lastmodified.sed
@@ -0,0 +1,2 @@
+# Find "Last modified: yyyy-mm-dd" in index.gmi and update to today's date
+/Last modified: [0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}
--- publish.sh
+++ publish.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+
+# Find "Last modified: yyyy-mm-dd" in index.gmi and update to today's date
+timestamp=`date +"%Y-%m-%d"`
+sed -i "s/Last modified: [0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}/Last modified: $timestamp/g" index.gmi
+
+# Git update
 git add . && git commit -a -m "Update content" && git push origin master && git push raspberrypi master
